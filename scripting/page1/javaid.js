9a256f76e0e8184013072a9b311a45de0ed674d6
Adrian <azzadri007@gmail.com>
2019-05-26 02:10:27 +0100

    second

---
+++ scripting/page1/javaid.js
@@ -0,0 +1,294 @@
+
+/* Holding Id properties as variables */
+function hold(){
+
+  var A1 = document.getElementById("A1");
+  var A2 = document.getElementById("A2");
+  var A3 = document.getElementById("A3");
+  var A4 = document.getElementById("A4");
+
+  var B1 = document.getElementById("B1");
+  var B3 = document.getElementById("B3");
+  var B4 = document.getElementById("B4");
+  var B5 = document.getElementById("B5");
+  var B6 = document.getElementById("B6");
+  var B7 = document.getElementById("B7");
+  var B8 = document.getElementById("B8");
+
+  var E1 = document.getElementById("E1");
+  var E2 = document.getElementById("E2");
+  var E3 = document.getElementById("E3");
+  var E4 = document.getElementById("E4");
+  var E5 = document.getElementById("E5");
+  var E6 = document.getElementById("E6");
+  var E7 = document.getElementById("E7");
+  var E8 = document.getElementById("E8");
+  var E9 = document.getElementById("E9");
+  var E10 = document.getElementById("E10");
+  var E11 = document.getElementById("E11");
+  var E12 = document.getElementById("E12");
+  var E13 = document.getElementById("E13");
+  var E14 = document.getElementById("E14");
+  var E15 = document.getElementById("E15");
+  var E16 = document.getElementById("E16");
+  var E17 = document.getElementById("E17");
+  var E18 = document.getElementById("E18");
+  var E19 = document.getElementById("E19");
+  var E20 = document.getElementById("E20");
+
+}
+
+/*----- Setting colour for when click on center -----*/
+/*  Color of hexagons now stay  */
+function stick(){
+  hold();
+  document.getElementById("center_hex").style.backgroundColor = "#d7d7d7";
+
+  A1.style.backgroundColor = "#F2F2F2";
+  A2.style.backgroundColor = "#F2F2F2";
+  A3.style.backgroundColor = "#F2F2F2";
+  A4.style.backgroundColor = "#F2F2F2";
+  B1.style.backgroundColor = "#F2F2F2";
+  B3.style.backgroundColor = "#F2F2F2";
+  B4.style.backgroundColor = "#F2F2F2";
+  B5.style.backgroundColor = "#F2F2F2";
+  B6.style.backgroundColor = "#F2F2F2";
+  B7.style.backgroundColor = "#F2F2F2";
+  B8.style.backgroundColor = "#F2F2F2";
+
+  E1.style.backgroundColor = "#F2F2F2";
+  E2.style.backgroundColor = "#F2F2F2";
+  E3.style.backgroundColor = "#F2F2F2";
+  E4.style.backgroundColor = "#F2F2F2";
+  E5.style.backgroundColor = "#F2F2F2";
+  E6.style.backgroundColor = "#F2F2F2";
+  E7.style.backgroundColor = "#F2F2F2";
+  E8.style.backgroundColor = "#F2F2F2";
+  E9.style.backgroundColor = "#F2F2F2";
+  E10.style.backgroundColor = "#F2F2F2";
+  E11.style.backgroundColor = "#F2F2F2";
+  E12.style.backgroundColor = "#F2F2F2";
+  E13.style.backgroundColor = "#F2F2F2";
+  E14.style.backgroundColor = "#F2F2F2";
+  E15.style.backgroundColor = "#F2F2F2";
+  E16.style.backgroundColor = "#F2F2F2";
+  E17.style.backgroundColor = "#F2F2F2";
+  E18.style.backgroundColor = "#F2F2F2";
+  E19.style.backgroundColor = "#F2F2F2";
+  E20.style.backgroundColor = "#F2F2F2";
+
+}
+
+function hexA1(){
+/* #EE82EE #DA70D6 */
+  document.body.style.background = "#ff9eb5";
+  document.getElementById("center_hex").style.visibility = "hidden";
+
+    A1.style.backgroundColor = "#ff9eb5";
+    A2.style.backgroundColor = "#ff9eb5";
+    A3.style.backgroundColor = "#ff9eb5";
+    A4.style.backgroundColor = "#ff9eb5";
+    B1.style.backgroundColor = "#ff9eb5";
+    B3.style.backgroundColor = "#ff9eb5";
+    B4.style.backgroundColor = "#ff9eb5";
+    B5.style.backgroundColor = "#ff9eb5";
+    B6.style.backgroundColor = "#ff9eb5";
+    B7.style.backgroundColor = "#ff9eb5";
+    B8.style.backgroundColor = "#ff9eb5";
+
+    E1.style.backgroundColor = "#ff9eb5";
+    E2.style.backgroundColor = "#ff9eb5";
+    E3.style.backgroundColor = "#ff9eb5";
+    E4.style.backgroundColor = "#ff9eb5";
+    E5.style.backgroundColor = "#ff9eb5";
+    E6.style.backgroundColor = "#ff9eb5";
+    E7.style.backgroundColor = "#ff9eb5";
+    E8.style.backgroundColor = "#ff9eb5";
+    E9.style.backgroundColor = "#ff9eb5";
+    E10.style.backgroundColor = "#ff9eb5";
+    E11.style.backgroundColor = "#ff9eb5";
+    E12.style.backgroundColor = "#ff9eb5";
+    E13.style.backgroundColor = "#ff9eb5";
+    E14.style.backgroundColor = "#ff9eb5";
+    E15.style.backgroundColor = "#ff9eb5";
+    E16.style.backgroundColor = "#ff9eb5";
+    E17.style.backgroundColor = "#ff9eb5";
+    E18.style.backgroundColor = "#ff9eb5";
+    E19.style.backgroundColor = "#ff9eb5";
+    E20.style.backgroundColor = "#ff9eb5";
+
+    setTimeout(function(){A1.style.display = "none";
+    A2.style.display = "none";
+    A3.style.display = "none";
+    A4.style.display = "none";}, 1800);
+    setTimeout(function(){window.location.href="pageA1.html"}, 2700);
+}
+function hexA2(){
+/* #EE82EE #DA70D6 */
+document.body.style.background = "#aadd77";
+  document.getElementById("center_hex").style.visibility = "hidden";
+
+    A1.style.backgroundColor = "#aadd77";
+    A2.style.backgroundColor = "#aadd77";
+    A3.style.backgroundColor = "#aadd77";
+    A4.style.backgroundColor = "#aadd77";
+    B1.style.backgroundColor = "#aadd77";
+    B3.style.backgroundColor = "#aadd77";
+    B4.style.backgroundColor = "#aadd77";
+    B5.style.backgroundColor = "#aadd77";
+    B6.style.backgroundColor = "#aadd77";
+    B7.style.backgroundColor = "#aadd77";
+    B8.style.backgroundColor = "#aadd77";
+
+    E1.style.backgroundColor = "#aadd77";
+    E2.style.backgroundColor = "#aadd77";
+    E3.style.backgroundColor = "#aadd77";
+    E4.style.backgroundColor = "#aadd77";
+    E5.style.backgroundColor = "#aadd77";
+    E6.style.backgroundColor = "#aadd77";
+    E7.style.backgroundColor = "#aadd77";
+    E8.style.backgroundColor = "#aadd77";
+    E9.style.backgroundColor = "#aadd77";
+    E10.style.backgroundColor = "#aadd77";
+    E11.style.backgroundColor = "#aadd77";
+    E12.style.backgroundColor = "#aadd77";
+    E13.style.backgroundColor = "#aadd77";
+    E14.style.backgroundColor = "#aadd77";
+    E15.style.backgroundColor = "#aadd77";
+    E16.style.backgroundColor = "#aadd77";
+    E17.style.backgroundColor = "#aadd77";
+    E18.style.backgroundColor = "#aadd77";
+    E19.style.backgroundColor = "#aadd77";
+    E20.style.backgroundColor = "#aadd77";
+
+    setTimeout(function(){A1.style.display = "none";
+    A2.style.display = "none";
+    A3.style.display = "none";
+    A4.style.display = "none";}, 1800);
+}
+function hexA3(){
+/* #EE82EE #DA70D6 */
+document.body.style.background = "#aec6cf";
+  document.getElementById("center_hex").style.visibility = "hidden";
+
+    A1.style.backgroundColor = "#aec6cf";
+    A2.style.backgroundColor = "#aec6cf";
+    A3.style.backgroundColor = "#aec6cf";
+    A4.style.backgroundColor = "#aec6cf";
+    B1.style.backgroundColor = "#aec6cf";
+    B3.style.backgroundColor = "#aec6cf";
+    B4.style.backgroundColor = "#aec6cf";
+    B5.style.backgroundColor = "#aec6cf";
+    B6.style.backgroundColor = "#aec6cf";
+    B7.style.backgroundColor = "#aec6cf";
+    B8.style.backgroundColor = "#aec6cf";
+
+    E1.style.backgroundColor = "#aec6cf";
+    E2.style.backgroundColor = "#aec6cf";
+    E3.style.backgroundColor = "#aec6cf";
+    E4.style.backgroundColor = "#aec6cf";
+    E5.style.backgroundColor = "#aec6cf";
+    E6.style.backgroundColor = "#aec6cf";
+    E7.style.backgroundColor = "#aec6cf";
+    E8.style.backgroundColor = "#aec6cf";
+    E9.style.backgroundColor = "#aec6cf";
+    E10.style.backgroundColor = "#aec6cf";
+    E11.style.backgroundColor = "#aec6cf";
+    E12.style.backgroundColor = "#aec6cf";
+    E13.style.backgroundColor = "#aec6cf";
+    E14.style.backgroundColor = "#aec6cf";
+    E15.style.backgroundColor = "#aec6cf";
+    E16.style.backgroundColor = "#aec6cf";
+    E17.style.backgroundColor = "#aec6cf";
+    E18.style.backgroundColor = "#aec6cf";
+    E19.style.backgroundColor = "#aec6cf";
+    E20.style.backgroundColor = "#aec6cf";
+
+    setTimeout(function(){A1.style.display = "none";
+    A2.style.display = "none";
+    A3.style.display = "none";
+    A4.style.display = "none";}, 1800);
+}
+
+function hexA4(){
+/* #EE82EE #DA70D6 */
+document.body.style.background = "#fdfd96";
+  document.getElementById("center_hex").style.visibility = "hidden";
+
+    A1.style.backgroundColor = "#fdfd96";
+    A2.style.backgroundColor = "#fdfd96";
+    A3.style.backgroundColor = "#fdfd96";
+    A4.style.backgroundColor = "#fdfd96";
+    B1.style.backgroundColor = "#fdfd96";
+    B3.style.backgroundColor = "#fdfd96";
+    B4.style.backgroundColor = "#fdfd96";
+    B5.style.backgroundColor = "#fdfd96";
+    B6.style.backgroundColor = "#fdfd96";
+    B7.style.backgroundColor = "#fdfd96";
+    B8.style.backgroundColor = "#fdfd96";
+
+    E1.style.backgroundColor = "#fdfd96";
+    E2.style.backgroundColor = "#fdfd96";
+    E3.style.backgroundColor = "#fdfd96";
+    E4.style.backgroundColor = "#fdfd96";
+    E5.style.backgroundColor = "#fdfd96";
+    E6.style.backgroundColor = "#fdfd96";
+    E7.style.backgroundColor = "#fdfd96";
+    E8.style.backgroundColor = "#fdfd96";
+    E9.style.backgroundColor = "#fdfd96";
+    E10.style.backgroundColor = "#fdfd96";
+    E11.style.backgroundColor = "#fdfd96";
+    E12.style.backgroundColor = "#fdfd96";
+    E13.style.backgroundColor = "#fdfd96";
+    E14.style.backgroundColor = "#fdfd96";
+    E15.style.backgroundColor = "#fdfd96";
+    E16.style.backgroundColor = "#fdfd96";
+    E17.style.backgroundColor = "#fdfd96";
+    E18.style.backgroundColor = "#fdfd96";
+    E19.style.backgroundColor = "#fdfd96";
+    E20.style.backgroundColor = "#fdfd96";
+
+    setTimeout(function(){A1.style.display = "none";
+    A2.style.display = "none";
+    A3.style.display = "none";
+    A4.style.display = "none";}, 1800);
+}
+
+function hide(){
+  document.getElementById("center_hex").style.visibility = "hidden";
+
+  A1.style.display = "none";
+  A2.style.display = "none";
+  A3.style.display = "none";
+  A4.style.display = "none";
+
+    B1.style.visibility = "hidden";
+    B3.style.visibility = "hidden";
+    B4.style.visibility = "hidden";
+    B5.style.visibility = "hidden";
+    B6.style.visibility = "hidden";
+    B7.style.visibility = "hidden";
+    B8.style.visibility = "hidden";
+
+    E1.style.visibility = "hidden";
+    E2.style.visibility = "hidden";
+    E3.style.visibility = "hidden";
+    E4.style.visibility = "hidden";
+    E5.style.visibility = "hidden";
+    E6.style.visibility = "hidden";
+    E7.style.visibility = "hidden";
+    E8.style.visibility = "hidden";
+    E9.style.visibility = "hidden";
+    E10.style.visibility = "hidden";
+    E11.style.visibility = "hidden";
+    E12.style.visibility = "hidden";
+    E13.style.visibility = "hidden";
+    E14.style.visibility = "hidden";
+    E15.style.visibility = "hidden";
+    E16.style.visibility = "hidden";
+    E17.style.visibility = "hidden";
+    E18.style.visibility = "hidden";
+    E19.style.visibility = "hidden";
+    E20.style.visibility = "hidden";
+
+}
