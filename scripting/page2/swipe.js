c6b4dd7ca7a1fa3d8e2325aa026952d1f04af035
azzadri007 <azzadri007@gmail.com>
2020-04-08 14:45:34 +0100

    SlidingToFroDetailedReportsDone

--- scripting/page2/swipe.js
+++ scripting/page2/swipe.js
@@ -1,61 +1,46 @@
 $(document).ready(function (){
-  $("#html, body").animate({ scrollTop: 0 }, 1);
+  $("#D5Container,#WebContainer").animate({ scrollTop: 0 }, 1);
 /* Click on R2 and all the pannels slide up and the progress bar (side pannel) appears 
       as well as main bar moving to left */
       $(document).on('click', '.bar_R2', function () {
-        $(".bar_R1, .bar_L1, .bar_L2, .bar_MR, .bar_ML, .title_Box").animate({'top':"-=600px"}, 1000);
+        $(".bar_R1, .bar_L1, .bar_L2, .bar_MR, .bar_ML, .title_Box").animate({'top':"-2000px"}, 1000);
         $(".bar_R2").delay(1000).animate({'margin-right':"375px"}, 1200);
         $("#bar_Progress").delay(1800).animate({'left':"-10px"}, 500);
         //Do again so all the bars are fully out the picture(sometimes this bugs out)
         $(".bar_R1, .bar_L1, .bar_L2, .bar_MR, .bar_ML, .title_Box").delay(1000).animate({'top':"-=600px"}, 1000);
        
         $("#D5Container").delay(1000).animate({'left':"50%"}, 2000);
-        $("body").delay(1100).queue(function (next) { $(this).css({'overflow-y': 'visible'});next(); });
       });
       /* Doing the same for the Right side */
       $(document).on('click', '.bar_R1', function () {
-        $(".bar_R2, .bar_L1, .bar_L2, .bar_MR, .bar_ML, .title_Box").animate({'top':"-=600px"}, 1000);
+        $(".bar_R2, .bar_L1, .bar_L2, .bar_MR, .bar_ML, .title_Box").animate({'top':"-2000px"}, 1000);
         $(".bar_R1").delay(1000).animate({'margin-right':"375px"}, 1200);
         $("#bar_Progress").delay(1800).animate({'left':"-10px"}, 500);
         $(".bar_R2, .bar_L1, .bar_L2, .bar_MR, .bar_ML, .title_Box").delay(1000).animate({'top':"-=600px"}, 1000);
-      
-        $("body").delay(1100).queue(function (next) { $(this).css({'overflow-y': 'visible'});next(); });
-      
       });$(document).on('click', '.bar_MR', function () {
-        $(".bar_R1, .bar_L1, .bar_R2, .bar_L2, .bar_ML, .title_Box").animate({'top':"-=600px"}, 1000);
+        $(".bar_R1, .bar_L1, .bar_R2, .bar_L2, .bar_ML, .title_Box").animate({'top':"-2000px"}, 1000);
         $(".bar_MR").delay(1000).animate({'margin-right':"375px", 'top':"50px"}, 1200);
         $("#bar_Progress").delay(1800).animate({'left':"-10px"}, 500);
         $(".bar_R1, .bar_L1, .bar_R2, .bar_ML, .bar_L2, .title_Box").delay(1000).animate({'top':"-=600px"}, 1000);
-      
-        $("body").delay(1100).queue(function (next) { $(this).css({'overflow-y': 'visible'});next(); });
-      
       });
       
       //Left Side
       $(document).on('click', '.bar_L1', function () {
-        $(".bar_R1, .bar_R2, .bar_L2, .bar_MR, .bar_ML, .title_Box").animate({'top':"-=600px"}, 1000);
+        $(".bar_R1, .bar_R2, .bar_L2, .bar_MR, .bar_ML, .title_Box").animate({'top':"-2000px"}, 1000);
         $(".bar_L1").delay(1000).animate({'margin-left':"-580px", 'top':"12px"}, 1200);
         $("#bar_Progress").delay(1800).animate({'left':"-10px"}, 500);
         $(".bar_R1, .bar_R2, .bar_L2, .bar_MR, .bar_ML, .title_Box").delay(1000).animate({'top':"-=600px"}, 1000);
-      
-        $("body").delay(1100).queue(function (next) { $(this).css({'overflow-y': 'visible'});next(); });
-      
       });$(document).on('click', '.bar_L2', function () {
-        $(".bar_R1, .bar_L1, .bar_R2, .bar_MR, .bar_ML, .title_Box").animate({'top':"-=600px"}, 1000);
+        $(".bar_R1, .bar_L1, .bar_R2, .bar_MR, .bar_ML, .title_Box").animate({'top':"-2000px"}, 1000);
         $(".bar_L2").delay(1000).animate({'margin-left':"-580px", 'top':"12px"}, 1200);
         $("#bar_Progress").delay(1800).animate({'left':"-10px"}, 500);
-        $(".bar_R1, .bar_L1, .bar_R2, .bar_MR, .bar_ML, .title_Box").delay(1000).animate({'top':"-=600px"}, 1000);
-        
-        $("body").delay(1100).queue(function (next) { $(this).css({'overflow-y': 'visible'});next(); });
+        $(".bar_R1, .bar_L1, .bar_R2, .bar_MR, .bar_ML, .title_Box").delay(1000).animate({'top':"-=2000px"}, 1000);
         $("#WebContainer").delay(800).animate({'left':"50%"}, 2000);
       });$(document).on('click', '.bar_ML', function () {
-        $(".bar_R1, .bar_L1, .bar_R2, .bar_MR, .bar_L2, .title_Box").animate({'top':"-=600px"}, 1000);
+        $(".bar_R1, .bar_L1, .bar_R2, .bar_MR, .bar_L2, .title_Box").animate({'top':"-2000px"}, 1000);
         $(".bar_ML").delay(1000).animate({'margin-left':"-580px",'top':"50px"}, 1200);
         $("#bar_Progress").delay(1800).animate({'left':"-10px"}, 500);
         $(".bar_R1, .bar_L1, .bar_R2, .bar_MR, .bar_L2, .title_Box").delay(1000).animate({'top':"-=600px"}, 1000);
-      
-        $("body").delay(1100).queue(function (next) { $(this).css({'overflow-y': 'visible'});next(); });
-      
       });
       
       $(document).on('click', '#color_Progress', function () {
@@ -69,7 +54,7 @@ $(document).ready(function (){
         $('#bar_Progress').delay(1800).animate({'left':"-80px"}, 500);
       
         $(".bar_L2").delay(800).animate({'margin-left':"-442.58px", 'top':"12px"}, 1200);
-        $(".bar_L1").delay(800).animate({'margin-left':"-669.34px", 'top':"50px"}, 1200);
+        $(".bar_L1").delay(800).animate({'margin-left':"-669.34px", 'top':"12px"}, 1200);
       
         $(".bar_R2").delay(800).animate({'margin-right':"-442.58px", 'top':"12px"}, 1200);
         $(".bar_R1").delay(800).animate({'margin-right':"-669.34px", 'top':"12px"}, 1200);
@@ -82,15 +67,15 @@ $(document).ready(function (){
         $("body").css('overflow', 'hidden');
         $("#D5Container").animate({'left':"200%"}, 1200);
         $("#WebContainer").animate({'left':"200%"}, 1200);
-        $("#html, body").delay(2000).animate({ scrollTop: 0 }, 1500);
+        $("#D5Container,#WebContainer").delay(2000).animate({ scrollTop: 0 }, 1500);
       });
       
       $(document).on('click', '#to_Title', function () {
-        $("#html, body").animate({ scrollTop: 0 }, 1500);
+        $("#D5Container,#WebContainer").animate({ scrollTop: 0 }, 1500);
       });
       
       $(document).on('click', '#to_Bottom', function () {
-        $("#html, body").animate({ scrollTop: $(document).height()}, 1500);
+        $("##D5Container,#WebContainer").animate({ scrollTop: $(document).height()}, 1500);
       });
 
 });
